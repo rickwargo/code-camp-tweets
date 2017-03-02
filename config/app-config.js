5b5ac64c3ab0af940dc450f0b21357871823ffec
rickwargo <github@epicminds.com>
2017-03-02 17:58:24 -0500

    Bring up-to-date with alexa-app-template

--- config/app-config.js
+++ config/app-config.js
@@ -2,7 +2,7 @@
 'use strict';
 
 module.exports = {
-    applicationId: 'amzn1.ask.skill.44be0769-b790-43b7-a834-d3c474b85e59',   // Must update or all calls will fail on appIntent.pre()
+    applicationId: 'amzn1.ask.skill.00000000-0000-0000-0000-000000000000', // Don't update here, update applicationid.json
     applicationName: 'code-camp-tweets',     // Must update this - no spaces, should be a valid identifier (hypens ok)
     functionName: 'Code-Camp-Tweets',        // Must update or gulp test-lambda will fail
     description: 'Retrieve tweets about Philly Code Camp'
