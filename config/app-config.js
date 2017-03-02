4e7b5fadcc51a2f34d33e8f2f5f310469f33d4a0
rickwargo <github@epicminds.com>
2017-03-02 18:13:54 -0500

    Bring up-to-date with alexa-app-template

--- config/app-config.js
+++ config/app-config.js
@@ -1,12 +1,18 @@
 /*jslint node: true */
 'use strict';
 
-module.exports = {
+var appConfig = {
     applicationId: 'amzn1.ask.skill.00000000-0000-0000-0000-000000000000', // Don't update here, update applicationid.json
-    applicationName: 'code-camp-tweets',     // Must update this - no spaces, should be a valid identifier (hypens ok)
-    functionName: 'Code-Camp-Tweets',        // Must update or gulp test-lambda will fail
+    applicationName: 'code-camp-tweets',
+    functionName: 'Code-Camp-Tweets',
     description: 'Retrieve tweets about Philly Code Camp'
 };
 
+// Allow applicationId to be held in separate file
+var appId = require('./applicationid.json');
+appConfig.applicationId = appId;
+
+
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
+module.exports = appConfig;
