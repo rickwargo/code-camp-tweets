6906b42a27ddfed13f8861c738a67a3e63c53678
rickwargo <github@epicminds.com>
2017-03-14 12:24:00 -0400

    Un-remove package.json from dist, get applicationId from package.json on startup

--- config/app-config.js
+++ config/app-config.js
@@ -2,15 +2,15 @@
 'use strict';
 
 var appConfig = {
-    applicationId: 'amzn1.ask.skill.00000000-0000-0000-0000-000000000000', // Don't update here, update applicationid.json
+    applicationId: 'amzn1.ask.skill.00000000-0000-0000-0000-000000000000', // Don't update here, update package.json
     applicationName: 'code-camp-tweets',
     functionName: 'Code-Camp-Tweets',
     description: 'Retrieve tweets about Philly Code Camp'
 };
 
 // Allow applicationId to be held in separate file
-var appId = require('./applicationid.json');
-appConfig.applicationId = appId.applicationId;
+var appId = require('../package.json');
+appConfig.applicationId = appId.alexa.applicationId;
 
 
 // Allow this module to be reloaded by hotswap when changed
